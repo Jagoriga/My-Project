0ce6044f5de8b8dc714987b429cffabe0e425f6b
LeoNe <jagoriga@mail.ru>
2021-09-06 17:43:50 +0300

    plus php

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my Project</title>
+    <link rel="stylesheet" href="./style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body class="open-sans">
+    
+    <header>
+        <div class="div-header">
+            <div class="social"> 
+                <a href="https://www.facebook.com/robotdreams.cc/" target="_blank" class="fa fa-facebook"></a>
+                <a href="https://www.instagram.com/robotdreams.cc/" target="_blank" class="fa fa-instagram"></a>
+                <a href="https://github.com/" target="_blank" class="fa fa-github"></a>
+            </div>
+            <nav class='navflex'>
+            
+             <ul>
+                 <li>
+                    <a href="#about">About</a> 
+                 </li>
+                 <li>
+                     <a href="#main">Main</a>
+                 </li>
+                 <li>
+                     <a href="#Contact">Contact Me</a>
+                 </li>
+             </ul>
+            </nav>
+        </div>
+    </header>
+
+     <!-- Jumbotron -->
+
+ <section class='jumbotron'>
+    <div class="container-home">
+        <div class="image-container">
+           
+                <img class="img-responsive" src="images/Photo.jpg" >
+            
+        </div>
+
+        <div class="text-container">
+            <h1>Leonid Gandler</h1>
+            <h2>Technical Support Manager.</h2>
+            <h3>Participation and Support for new installations. SW Updates, Maintenance. 
+                Technical 3rd line support, helpdesk. Technical training of partners, remote and on site.</h3>
+            <br>
+           <h2>leonid.gandler@gmail.com </h2> 
+
+        </div>
+    </div>
+</section>
+  <br><br>
+
+ <!-- About me -->
+
+ <section class="about-me" id='about'>
+    
+  <div class="container">  
+
+    <div class="section-header">
+        <span class='section-title'>About me.</span>
+        <span class='section-subtitle'>Below you can see the information about My Education, My Achievements and Experience, and My Hobbies.</span>
+    </div>
+         
+     
+    <div class="section-content">
+        <div class="section-item">
+            <h2>My Education.</h2>
+            <p >You can see information about my education in this section. Information about the courses I have completed. Availability of certificates and diplomas</p>
+            <button class="button" onclick="document.location='./education.html'">READ MORE</button>
+        </div>
+        <div class="section-item">
+            <h2>My Achievements and Experience.</h2>
+            <p>Here you can see information about my previous and current jobs.
+                You can also see information about my participation in various projects.</p>
+            <button class="button" onclick="document.location='./workexperience.html'">READ MORE</button>
+        </div>
+        <div class="section-item">
+            <h2>My Hobbies.</h2>
+            <p>You can see information about my hobbies and additional skills in this section.</p>
+            <button class="button" onclick="document.location='./hobbies.html'">READ MORE</button>
+            
+        </div>
+
+    </div>
+  </div>       
+
+ <!-- Main -->
+
+</section>
+<br>
+<section id='main'>
+    <div class="section-header">
+            <span class='section-title'>Main.</span>
+            <span class='section-subtitle'>Below you can see the some main information about me.</span>
+    </div>
+    <div class='div-grid'>
+        <div class='div1'>
+            <ul>
+                <li><a href="#project1">Project 1.</a></li>
+                <li><a href="#project2">Project 2.</a></li>
+                <li><a href="#project3">Project 3.</a></li>   
+            </ul>
+        </div>
+            
+        <div class="div2" >
+            <div class="container-home" id='project1'>
+                <div class="image-container">
+                   
+                    <img  src="images/azimut.jpg" class="img-grid" >
+                    
+                </div>
+        
+                <div class="text-container">
+                    <h2>VALSET apartments by AZIMUT Rosa Khutor.</h2>
+                    <p>The apart-hotel AZIMUT is located in the center of the Russian main ski resort Rosa Khutor, within walking distance from the ski lift. Next to the VALSET apartments by AZIMUT Rosa Khutor you will find a beach area, a yoga and massage center, shops and other entertainment for children and adults.</p>
+                    <p>Due to convenient transport connection and free shuttles between the resort of Rosa Khutor and Sochi you will be able to make daily visits to the Black Sea, participate in various excursions, visit the Olympic Park or adventure park SKYPARK AJ Hackett Sochi, attend concerts or sports competitions in the vicinity of Sochi.</p>
+                    <p>Aparthotel VALSET apartments by AZIMUT Rosa Khutor offers apartments of different types and price categories. Among them you will find both compact rooms with a combined kitchen and living area, as well as large spaces with separate bedrooms and kitchen.</p>
+                    <br>
+                   
+        
+                </div>
+            </div>
+
+            <div class="container-home" id='project2'>
+                <div class="image-container">
+                   
+                    <img  src="images/Golden sands.jpg" class="img-grid" >
+                    
+                </div>
+        
+                <div class="text-container">
+                    <h2>Radisson Blu Resort & Spa, Malta Golden Sand.</h2>
+                    <p>Set into the rocky coast of Malta's Golden Bay, the Radisson Blu Resort & Spa includes a large swimming pool, a lagoon pool and a children's pool. It also offers a private beach and a spa. Rooms are large and luxurious.</p>
+                    <p>Radisson Blu Resort & Spa, Malta Golden Sands enjoys a quiet, private setting, with peaceful views of the sea and the surrounding countryside. It is a 10 minute drive from the harbor where ferries leave for Gozo.</p>
+                    <p>The spa extends over 10700 ft² of space, and the resort's leisure center offers stunning views of the sea from its large windows overlooking the bay.
+                        Rooms are furnished with modern decor and offer a balcony with sea or countryside views. During summer the rate includes one sun umbrella and 2 lounge chairs per room.</p>
+                    <br>
+                   
+        
+                </div>
+            </div>
+
+            <div class="container-home" id='project3'>
+                <div class="image-container">
+                   
+                    <img  src="images/premier.jpg" class="img-grid" >
+                    
+                </div>
+        
+                <div class="text-container">
+                    <h2>Premier Palace Hotels. </h2>
+                    <p>Premier Palace Hotel – Kyiv-featured both in essence and atmosphere – is a luxurious historic hotel, the crown jewel in Premier Hotels and Resorts collection. Its history is unique and closely connected with the history of Kyiv. This is one of the capital attractions; it is worth taking some time to visit it.</p>
+                    <p>Staying at the Premier Palace Hotel, means living in the heart of Kyiv. Premier Palace Hotel is a hotel in the centre of Kyiv.  It is only 100 meters from Khreshchatyk – the main street of the capital.  From the windows of its suits and restaurants, you can see the well-known golden-domed temples: St  Volodymyr's, St. Michael's, and Saint Sophia’s Cathedrals.  Within walking distance, there are several art galleries, museums, parks, shopping and business centers.  From the upper floors, the guests enjoy a breathtaking panorama of Khreshchatyk:  all the way from Bessarabska square to the Friendship of Nations Arch.</p>
+                    
+                    <br>
+                   
+        
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<!-- Contact Me -->
+
+<section class='contacts' id='Contact'>
+        <div class="contact-container">
+            
+            <div class="section-header">
+                <span class='section-title'>Contact Me</span>
+                <span class='section-subtitle'>You can use this form to contact me. You can leave your comment and I will contact you.</span>
+            </div>
+
+            <form method="post" class="contacts-form">
+                <div class="contacts-form__inputs">
+                        <input type="text" name='username' placeholder="Your name">
+                        <input type="email" name='email' placeholder="Your Email">
+                </div>
+                <br>
+                    <textarea name="message" cols="30" rows="5" placeholder="Write Message"></textarea>
+                    <button class="button" type='submit' name='sub'>Send</button>
+                 <div id='resultmsg'></div> 
+                 <br>   
+            </form>
+            
+        </div>
+</section>
+<footer>
+    <span>Не судите строго! :-) &copy; 2021</span>
+</footer>
+
+<script src="./formhandler.js" type="text/javascript"></script>
+
+<?php
+$urok="Mail from my website";
+error_reporting( E_ERROR );   //Отключение предупреждений и нотайсов (warning и notice) на сайте
+// создание переменных из полей формы		
+if (isset($_POST['username']))		{$username		= $_POST['username'];		if ($username == '')	{unset($username);}}
+if (isset($_POST['email']))		    {$email		    = $_POST['email'];		    if ($email == '')	    {unset($email);}}
+if (isset($_POST['message']))		{$message		= $_POST['message'];		if ($message == '')	    {unset($text);}}
+if (isset($_POST['sab']))			{$sab			= $_POST['sab'];		    if ($sab == '')		    {unset($sab);}}
+//стирание треугольных скобок из полей формы
+/* комментарий */
+if (isset($username) ) {
+$username=stripslashes($username);
+$username=htmlspecialchars($username);
+}
+if (isset($email) ) {
+$email1=stripslashes($email);
+$email1=htmlspecialchars($email);
+}
+if (isset($message) ) {
+$message=stripslashes($message);
+$message=htmlspecialchars($message);
+}
+// адрес почты куда придет письмо
+$address="leonid.gandler@gmail.com";
+// текст письма 
+$note_text="Тема : $urok \r\nИмя : $username \r\n Email : $email \r\n Дополнительная информация : $message";
+
+if (isset($username)  &&  isset ($sab) ) {
+mail($address,$note_text,"Content-type:text/plain; windows-1251"); 
+// сообщение после отправки формы
+    
+echo "<p style='color:green;'>Уважаемый(ая) <b style='color:red;'>$username</b> Ваше письмо отправленно успешно. <br> Спасибо. <br>Вам скоро ответят на почту <b style='color:red;'> $email1</b>.</p>";
+}
+
+?>
+
+</body>
+</html>
